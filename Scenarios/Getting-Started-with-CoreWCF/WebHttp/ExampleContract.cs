8003ce69dcfdaf385254cda6820bf935e4b8281f
Sam Spencer <54915162+samsp-msft@users.noreply.github.com>
2022-05-13 14:23:48 -0700

    Ported across the WebHttp example and created a client for it. (#8)
    * Ported across the WebHttp example and created a client for it.
    
    * cleaning up the contract definition, and client code instantiating the contract properties.
    
    * Updated based on review feedback
    
    * Added a readme to talk about the client generation
    
    * phrasing
    
    Co-authored-by: Sam Spencer <samsp@microsoft.com>

---
+++ Scenarios/Getting-Started-with-CoreWCF/WebHttp/ExampleContract.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using CoreWCF.OpenApi.Attributes;
+
+namespace WebHttp
+{
+    [DataContract(Name = "ExampleContract", Namespace = "http://example.com")]
+    internal class ExampleContract
+    {
+        [DataMember(Name = "SimpleProperty", Order = 1)]
+        [OpenApiProperty(Description = "SimpleProperty description.")]
+        public string SimpleProperty { get; set; }
+
+        [DataMember(Name = "ComplexProperty", Order = 2)]
+        [OpenApiProperty(Description = "ComplexProperty description.")]
+        public InnerContract ComplexProperty { get; set; }
+
+        [DataMember(Name = "SimpleCollection", Order = 3)]
+        [OpenApiProperty(Description = "SimpleCollection description.")]
+        public List<string> SimpleCollection { get; set; }
+
+
+        [DataMember(Name = "ComplexCollection", Order = 4)]
+        [OpenApiProperty(Description = "ComplexCollection description.")]
+        public List<InnerContract> ComplexCollection { get; set; }
+    }
+
+    [DataContract(Name = "InnerContract", Namespace = "http://example.com")]
+    internal class InnerContract
+    {
+        [DataMember(Name = "Name", Order = 1)]
+        [OpenApiProperty(Description = "Name description.")]
+        public string Name { get; set; }
+    }
+}
