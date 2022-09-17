66cdc730e61a9486fe3720cce001065326a9fdac
Abdelwahab Afifi <v-aafifi@microsoft.com>
2022-09-17 06:41:27 +0200

    Add BasicHttpBinding Sample - IIS Hosted

---
+++ Basic/Binding/Basic/Http/CS/Service/ICalculatorService.cs
@@ -0,0 +1,19 @@
+﻿
+using CoreWCF;
+
+namespace CoreWcf.Samples.Http
+{
+    // Define a service contract.
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+}
---
+++ Basic/Binding/Basic/Http/CS/Service/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace CoreWcf.Samples.Http
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebHost host = CreateWebHostBuilder(args).Build();
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+            .UseIIS()
+            .UseStartup<BasicHttpBindingStartup>();
+    }
+}
