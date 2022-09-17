23271eacab2dd9a06c3d6c38e57c157e034062c8
Abdelwahab Afifi <v-aafifi@microsoft.com>
2022-09-17 06:41:27 +0200

    update code
    convert to top level-statement app and add license
    
    Add BasicHttpBinding Transport Security Sample - IIS/Kestrel Hosted
    
    Updates:
    1. Remove CS folder in folder heirarchy
    2. Rename .sln file to Binding.Basic.Http.sln
    3. Remove .config folder (validate this doesn't break the build)
    
    Update Client Connected Service URL
    
    Update LaunchSettings and remove .UseIIS (WebHost default)
    
    Update Client NuGet Packages
    
    Update NuGet pPackages
    
    Add BasicHttpBinding Sample - IIS Hosted

---
+++ Basic/Binding/Basic/TransportSecurity/Service/CalculatorService.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace CoreWcf.Samples.TransportSecurity
+{
+    // Service class which implements the service contract interface.
+    public class CalculatorService : ICalculatorService
+    {
+        public double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
