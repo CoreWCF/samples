2cfd3efc3858dbd64a1517510f7454aa294122f4
afifi-ins <89879907+afifi-ins@users.noreply.github.com>
2022-10-11 20:20:32 +0200

    Add BasicHttpBinding Sample - IIS Hosted (#24)
    * Add BasicHttpBinding Sample - IIS Hosted

---
+++ Basic/Binding/Basic/Http/Service/CalculatorService.cs
@@ -0,0 +1,27 @@
+﻿namespace CoreWcf.Samples.Http
+{
+    // Service class which implements the service contract interface.
+    public class CalculatorService : ICalculatorService
+    {
+        public double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+
+}
---
+++ Basic/Binding/Basic/Http/Service/Startup.cs
@@ -0,0 +1,32 @@
+﻿using CoreWCF;
+using CoreWCF.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreWcf.Samples.Http
+{
+    public class BasicHttpBindingStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Enable CoreWCF Services, with metadata (WSDL) support
+            services.AddServiceModelServices()
+                .AddServiceModelMetadata();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseServiceModel(builder =>
+            {
+                // Add the Calculator Service
+                builder.AddService<CalculatorService>(serviceOptions => { })
+                // Add BasicHttpBinding endpoint
+                .AddServiceEndpoint<CalculatorService, ICalculatorService>(new BasicHttpBinding(), "/CalculatorService/basicHttp");
+
+                // Configure WSDL to be available
+                var serviceMetadataBehavior = app.ApplicationServices.GetRequiredService<CoreWCF.Description.ServiceMetadataBehavior>();
+                serviceMetadataBehavior.HttpGetEnabled = true;
+            });
+        }
+    }
+}
