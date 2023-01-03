30ca154ce6b9a3486e09f4199ddc9dfba8ee6fe7
g7ed6e <guillaume.delahaye@outlook.com>
2023-01-03 22:40:42 +0100

    Remove template comment

--- Scenarios/Authentication/Minimal-machine-to-machine-using-JWT/Client/Program.cs
+++ Scenarios/Authentication/Minimal-machine-to-machine-using-JWT/Client/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Net;
+﻿using System.Net;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using IdentityModel.Client;
