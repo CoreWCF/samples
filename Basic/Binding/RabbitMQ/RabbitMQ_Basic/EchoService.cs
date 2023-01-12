5f646999771adbb8fe34d519bbdfd1a9d566ca3b
Jon Louie <louiejon@amazon.com>
2023-01-11 20:23:05 -0800

    Add rabbitmq sample projects

---
+++ Basic/Binding/RabbitMQ/RabbitMQ_Basic/EchoService.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace RabbitMQ_Basic;
+
+public class EchoService : IEchoService
+{
+    public string Echo(string text)
+    {
+        System.Console.WriteLine($"Received {text} from client!");
+        return text;
+    }
+
+    public string ComplexEcho(EchoMessage text)
+    {
+        System.Console.WriteLine($"Received {text.Text} from client!");
+        return text.Text;
+    }
+
+    public string FailEcho(string text)
+        => throw new FaultException<EchoFault>(new EchoFault() { Text = "WCF Fault OK" }, new FaultReason("FailReason"));
+}
---
+++ Basic/Binding/RabbitMQ/RabbitMQ_Basic/IEchoService.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RabbitMQ_Basic
+{
+    [DataContract]
+    public class EchoFault
+    {
+        [DataMember]
+        [AllowNull]
+        public string Text { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IEchoService
+    {
+        [OperationContract]
+        string Echo(string text);
+
+        [OperationContract]
+        string ComplexEcho(EchoMessage text);
+
+        [OperationContract]
+        [FaultContract(typeof(EchoFault))]
+        string FailEcho(string text);
+    }
+
+    [DataContract]
+    public class EchoMessage
+    {
+        [AllowNull]
+        [DataMember]
+        public string Text { get; set; }
+    }
+}
