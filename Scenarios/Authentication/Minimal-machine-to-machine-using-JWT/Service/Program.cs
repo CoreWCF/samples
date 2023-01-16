2006c235d2a2f8dd7ebe89252d492cddd7bc9d2b
g7ed6e <guillaume.delahaye@outlook.com>
2023-01-16 10:10:34 +0100

    Add license headers

--- Scenarios/Authentication/Minimal-machine-to-machine-using-JWT/Service/Program.cs
+++ Scenarios/Authentication/Minimal-machine-to-machine-using-JWT/Service/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder();
@@ -13,7 +16,12 @@ builder.Services.AddAuthorization(options =>
 {
     options.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
         .RequireAuthenticatedUser()
-        .RequireClaim("scope", "api")
+        .RequireAssertion(context =>
+        {
+            string[] scopes = context.User.FindFirst("scope")?.Value.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                ?? Array.Empty<string>();
+            return scopes.Any(x => string.Equals(x, "api", StringComparison.Ordinal));
+        })
         .Build();
 });
 builder.Services.AddTransient<SecuredService>();
