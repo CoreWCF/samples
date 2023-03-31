01aa23f03190cc706c6403beeb782c8fb2eeb2f6
Abdelwahab Afifi <v-aafifi@microsoft.com>
2023-04-01 01:06:22 +0200

    Add Getting Started Sample

---
+++ Basic/GettingStarted/Service/Program.cs
@@ -0,0 +1,25 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+var builder = WebApplication.CreateBuilder();
+
+//Enable CoreWCF Services, with metadata (WSDL) support
+builder.Services.AddServiceModelServices()
+    .AddServiceModelMetadata()
+    .AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+
+var app = builder.Build();
+
+app.UseServiceModel(builder =>
+{
+    // Add the Calculator Service
+    builder.AddService<CalculatorService>(serviceOptions => { })
+    // Add BasicHttpBinding endpoint
+    .AddServiceEndpoint<CalculatorService, ICalculatorService>(new BasicHttpBinding(), "CalculatorService");
+
+    // Configure WSDL to be available
+    var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
+    serviceMetadataBehavior.HttpGetEnabled = true;
+});
+
+app.Run();
